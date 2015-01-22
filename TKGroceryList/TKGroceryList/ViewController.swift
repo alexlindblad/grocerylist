539ebe9c3f60648d5f94fe085923a493e29ab20c
Alex J Lindblad <alex@fsstudio.com>
2015-01-21 20:44:54 -0800

    add and hookup Parse

--- TKGroceryList/TKGroceryList/ViewController.swift
+++ TKGroceryList/TKGroceryList/ViewController.swift
@@ -21,5 +21,18 @@ class ViewController: UIViewController {
     }
 
 
+    override func viewDidAppear(animated: Bool) {
+        var gameScore = PFObject(className: "GameScore")
+        gameScore.setObject(1337, forKey: "score")
+        gameScore.setObject("Sean Plott", forKey: "playerName")
+        gameScore.saveInBackgroundWithBlock { 
+            (success: Bool!, error: NSError!) -> Void in
+            if success != nil {
+                NSLog("Object created with id: \(gameScore.objectId)")
+            } else {
+                NSLog("%@", error)
+            }
+        }
+    }
 }
 
