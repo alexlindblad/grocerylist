1d7e229d589ee31edd33d3a84339fd8fb8191349
Alex J Lindblad <alex@fsstudio.com>
2015-01-21 21:25:17 -0800

    incorporate crash analytics

--- TKGroceryList/TKGroceryList/AppDelegate.swift
+++ TKGroceryList/TKGroceryList/AppDelegate.swift
@@ -17,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        Parse.setApplicationId(Constants.Parse.AppID, clientKey: Constants.Parse.Key);
+        ParseCrashReporting.enable()
+        Parse.setApplicationId(Constants.Parse.AppID, clientKey: Constants.Parse.Key)
         PFAnalytics.trackAppOpenedWithLaunchOptionsInBackground(launchOptions, block: nil)
 
         return true
--- TKGroceryList/TKGroceryList/ViewController.swift
+++ TKGroceryList/TKGroceryList/ViewController.swift
@@ -20,19 +20,5 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
-    override func viewDidAppear(animated: Bool) {
-        var gameScore = PFObject(className: "GameScore")
-        gameScore.setObject(1337, forKey: "score")
-        gameScore.setObject("Sean Plott", forKey: "playerName")
-        gameScore.saveInBackgroundWithBlock { 
-            (success: Bool!, error: NSError!) -> Void in
-            if success != nil {
-                NSLog("Object created with id: \(gameScore.objectId)")
-            } else {
-                NSLog("%@", error)
-            }
-        }
-    }
 }
 
