7d8f30125f63f58901ce048f5fc39705ee76f2d0
Alex J Lindblad <alex@fsstudio.com>
2015-02-21 15:49:30 -0800

    store list item in edit vc

--- TKGroceryList/TKGroceryList/ConfirmAddGroceryListItemViewController.swift
+++ TKGroceryList/TKGroceryList/ConfirmAddGroceryListItemViewController.swift
@@ -13,37 +13,46 @@ class ConfirmAddGroceryListItemViewController : UIViewController {
     @IBOutlet weak var quantityTextField: UITextField!
     @IBOutlet weak var itemLabel: UILabel!
     
-    var item: GroceryItem!
+    var listItem: GroceryListItem!
     var quantity = 1 as NSNumber!
-    
-    class func forItem(item: GroceryItem) -> ConfirmAddGroceryListItemViewController {
+
+    class func createViewController() ->ConfirmAddGroceryListItemViewController {
         let storyboard = UIStoryboard(name: Constants.StoryBoardID.MainSBName, bundle: nil)
 
         let viewController = storyboard.instantiateViewControllerWithIdentifier(Constants.StoryBoardID.ConfirmAddListItem) as ConfirmAddGroceryListItemViewController
-        
-        viewController.item = item
+
+        return viewController
+    }
+    
+    class func forItem(item: GroceryItem) -> ConfirmAddGroceryListItemViewController {
+        let viewController = ConfirmAddGroceryListItemViewController.createViewController()
+        viewController.listItem = GroceryListItem()
+        viewController.listItem.item = item
+        viewController.listItem.quantity = 1
+        viewController.listItem.unitOfMeasure = ""
+        viewController.listItem.saveEventually()
         
         return viewController
     }
     
     
     override func viewWillAppear(animated: Bool) {
-        if item != nil {
-            itemLabel.text = item.name
+        if listItem != nil {
+            listItem.item.fetchIfNeeded()
+            itemLabel.text = listItem.item.name
+            quantity = listItem.quantity
+            quantityTextField.text = String(format:"%d", listItem.quantity)
         }
     }
     
     @IBAction func valueChanged(sender: UIStepper) {
-        self.quantity = NSNumber(double:sender.value)
+        quantity = NSNumber(double:sender.value)
+        listItem.quantity = quantity
         quantityTextField.text = String(format:"%d", quantity.integerValue)
     }
     
     @IBAction func addItem(sender: AnyObject) {
-        var groceryListItem = GroceryListItem()
-        groceryListItem.item = item
-        groceryListItem.quantity = quantity
-        groceryListItem.unitOfMeasure = ""
-        groceryListItem.saveEventually()
+        listItem.saveEventually()
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
